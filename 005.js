cbf9518656d16006356d5e31eae017b585cdeb94
rijalul <mrijal05@gmail.com>
2020-07-21 13:23:34 +0700

    edit 005.js

--- 005.js
+++ 005.js
@@ -4,12 +4,16 @@ prompt.start();
 prompt.get(['TahunPertama','TahunKedua'], function (err, result) {
     if (err) { return onErr(err); }
     var tahun_kabisat = Array();
+    var tahun_biasa   = Array();
     for (let index = result.TahunPertama; index <= result.TahunKedua; index++) {
         if (index % 4 == '0') {
             tahun_kabisat.push(index)
+        }else{
+            tahun_biasa.push(index)
         }
     }
-    console.log (tahun_kabisat)
+    console.log ('list tahun kabisat : ' +tahun_kabisat)
+    console.log ('list tahun biasa : ' +tahun_biasa)
     
 });
 
